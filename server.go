ad26db5c51bff165318524170ed04025016d294c
xiongjie zhu <aqaqzxsw123@qq.com>
2023-01-06 14:06:18 +0800

    用户消息广播功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -64,6 +65,28 @@ func (this *Server) Handler(conn net.Conn) {
 
 	//广播当前用户上线消息
 	this.BroadCast(user, "已上线")
+
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println(("Conn read erro:"), err)
+				return
+			}
+
+			//提取用户的消息（去除'\n')
+			msg := string(buf[:n-1])
+
+			//将得到的消息广播
+			this.BroadCast(user, msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {}
