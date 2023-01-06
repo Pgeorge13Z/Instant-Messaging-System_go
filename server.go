f2bdaba6966f784f0d37f0383e6ba3897937d171
pgeorge <aqaqzxsw123@qq.com>
2023-01-06 21:07:01 +0800

    V2-接受客户端发送的消息，并广播

--- server.go
+++ server.go
@@ -56,15 +56,10 @@ func (this *Server) Handler(conn net.Conn) {
 	//...当前链接的业务
 	//fmt.Println("链接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn, this)
 
-	//用户上线,将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
-
-	//广播当前用户上线消息
-	this.BroadCast(user, "已上线")
+	//用户上线
+	user.Online()
 
 	//接受客户端发送的消息
 	go func() {
@@ -72,7 +67,9 @@ func (this *Server) Handler(conn net.Conn) {
 		for {
 			n, err := conn.Read(buf)
 			if n == 0 {
-				this.BroadCast(user, "下线")
+
+				//用户下线
+				user.Offline()
 				return
 			}
 			if err != nil && err != io.EOF {
--- user.go
+++ user.go
@@ -7,15 +7,18 @@ type User struct {
 	Addr string
 	C    chan string
 	Conn net.Conn
+
+	server *Server
 }
 
 //创建获取用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	user := &User{
-		Name: conn.RemoteAddr().String(),
-		Addr: conn.RemoteAddr().String(),
-		C:    make(chan string),
-		Conn: conn,
+		Name:   conn.RemoteAddr().String(),
+		Addr:   conn.RemoteAddr().String(),
+		C:      make(chan string),
+		Conn:   conn,
+		server: server,
 	}
 
 	//驱动监听当前user channel消息的goroutine
@@ -23,6 +26,25 @@ func NewUser(conn net.Conn) *User {
 
 	return user
 
+}
+
+func (this *User) Online() {
+	//用户上线,将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.server.BroadCast(this, "已上线")
+}
+
+func (this *User) Offline() {
+	this.server.BroadCast(this, "已下线")
+}
+
+//消息处理
+func (this *User) Domsg(msg string) {
+	this.server.BroadCast(this, msg)
 }
 
 //监听当前User channel方法，一旦有消息，直接发送给客户端
